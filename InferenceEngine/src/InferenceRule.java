3733d971b4f34a061e22c14c563bf80f53b60e35
roaaeliwah <roaa.m.elsayed@gmail.com>
2025-10-29 20:31:12 +0300

    modus ponens logic

--- InferenceEngine/src/InferenceRule.java
+++ InferenceEngine/src/InferenceRule.java
@@ -1,4 +1,5 @@
 interface InferenceRule {
     boolean matches(Expression exp1, Expression exp2);
     Expression apply(Expression exp1, Expression exp2);
+    String name();
 }
--- InferenceEngine/src/ModusPonens.java
+++ InferenceEngine/src/ModusPonens.java
@@ -1,2 +1,24 @@
 public class ModusPonens implements InferenceRule{
+    @Override
+    public boolean matches(Expression exp1, Expression exp2) {
+        String e1 = exp1.getRepresentation();
+        String e2 = exp2.getRepresentation();
+        return (e1.contains(">") && e2.charAt(0) == e1.charAt(0) && e2.length() == 1)
+                || (e2.contains(">") && e1.charAt(0) == e2.charAt(0) && e1.length() == 1);
+    }
+
+    @Override
+    public Expression apply(Expression exp1, Expression exp2) {
+        String e1 = exp1.getRepresentation();
+        String e2 = exp2.getRepresentation();
+        if(matches(exp1, exp2)) {
+            return new Expression(Character.toString(e1.charAt(0)));
+        }
+        return null;
+    }
+
+    @Override
+    public String name() {
+        return "Modus Ponens";
+    }
 }
